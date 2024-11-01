0168d9f693020595793e332a59769b70cb3d93a1
Mitchell Christ <mitchell@human.marketing>
2024-10-31 21:32:07 -0700

    add styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,61 +2,80 @@
 
 import { useState } from 'react'
 import Input from '@/ui/Input'
-import { VscEye, VscFile, VscGithub } from 'react-icons/vsc'
+import { VscEye, VscFile, VscGithub, VscStarFull } from 'react-icons/vsc'
 import ClickToCopy from '@/ui/ClickToCopy'
 import Link from 'next/link'
+import { cn } from '@/lib/utils'
 
-const DEFAULT_REPO = 'nuotsu/github-iframe'
 const DEFAULT_PATH = 'src/app/globals.css'
 
 export default function Home() {
-	const [repo, setRepo] = useState(DEFAULT_REPO)
+	const [repo, setRepo] = useState('nuotsu/github-iframe')
 	const [path, setPath] = useState(DEFAULT_PATH)
 
-	const code = `<iframe src="https://github-iframe.vercel.app/${repo}/${path}"></iframe>`
+	const code = [
+		`<iframe`,
+		`\tsrc="https://github-iframe.vercel.app/${repo}/${path}"`,
+		`\twidth="100%"`,
+		`></iframe>`,
+	].join('\n')
+
+	const isValid = repo && path
 
 	return (
-		<>
+		<section className="mx-auto max-w-screen-sm space-y-6 p-4">
 			<header>
-				<h1>GitHub iframe</h1>
+				<h1 className="font-bold">GitHub iframe</h1>
+
+				<p>
+					<a
+						className="with-icon"
+						href="https://github.com/nuotsu/github-iframe"
+					>
+						<VscStarFull /> Star on GitHub
+					</a>
+				</p>
 			</header>
 
-			<section>
-				<fieldset>
-					<legend>Details</legend>
+			<fieldset>
+				<legend>Options</legend>
 
-					<div className="flex flex-wrap gap-x-4">
-						<Input
-							icon={VscGithub}
-							placeholder={DEFAULT_REPO}
-							defaultValue={repo}
-							onChange={(e) => setRepo(e.target.value)}
-						/>
+				<div className="grid gap-x-4 gap-y-2 md:grid-cols-2">
+					<Input
+						title="owner/repo"
+						icon={VscGithub}
+						defaultValue={repo}
+						onChange={(e) => setRepo(e.target.value)}
+						pattern=".+/.+"
+					/>
 
-						<Input
-							icon={VscFile}
-							placeholder={DEFAULT_PATH}
-							defaultValue={path}
-							onChange={(e) => setPath(e.target.value)}
-						/>
-					</div>
-				</fieldset>
+					<Input
+						title="path/to/file"
+						icon={VscFile}
+						defaultValue={path}
+						onChange={(e) => setPath(e.target.value)}
+					/>
+				</div>
+			</fieldset>
 
-				<pre>
-					<code>{code}</code>
+			<article className="group">
+				<pre className="overflow-x-auto border group-has-[button:hover]:border-black/30">
+					<code className={cn('block p-2', !isValid && 'opacity-50')}>
+						{isValid ? code : 'bruh...'}
+					</code>
 				</pre>
 
-				<div className="flex flex-wrap gap-2">
-					<ClickToCopy value={code}>Copy code</ClickToCopy>
-
+				<div className="flex flex-wrap justify-end gap-4">
 					<Link className="with-icon" href={`/${repo}/${path}`}>
 						<VscEye />
 						Fullscreen Preview
 					</Link>
-				</div>
 
-				<div dangerouslySetInnerHTML={{ __html: code }} />
-			</section>
-		</>
+					<ClickToCopy value={code}>Copy code</ClickToCopy>
+				</div>
+			</article>
+
+			{isValid && <div dangerouslySetInnerHTML={{ __html: code }} />}
+		</section>
 	)
 }
--- src/ui/Input.tsx
+++ src/ui/Input.tsx
@@ -2,12 +2,19 @@ import type { ComponentProps } from 'react'
 
 export default function Input({
 	icon: Icon,
+	title,
 	...props
 }: { icon: React.ElementType } & ComponentProps<'input'>) {
 	return (
-		<label className="with-icon cursor-pointer">
+		<label className="with-icon group cursor-pointer">
 			<Icon />
-			<input type="text" {...props} />
+			<input
+				className="grow border-b outline-none invalid:!border-red-500 focus:border-current group-hover:border-black/30"
+				type="text"
+				title={title}
+				placeholder={title}
+				{...props}
+			/>
 		</label>
 	)
 }
